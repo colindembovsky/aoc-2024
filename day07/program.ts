67a3937681f2d9a6bf72515dd9a8676b25055698
Colin Dembovsky <colindembovsky@github.com>
2024-12-07 11:40:51 -0600

    solve 7.1

--- day07/program.ts
+++ day07/program.ts
@@ -2,10 +2,35 @@ import { loadInput, dayName, Difficulty } from "../utils/readUtils";
 import { timedExecute } from "../utils/miscUtils";
 
 let day = dayName(__dirname);
-let contents = loadInput(__dirname, Difficulty.EASY);
+let contents = loadInput(__dirname, Difficulty.HARD);
 let lines = contents.split("\n");
 
-console.log(`==== ${day}: PART 1 ====`);
-timedExecute(() => {
+function checkIfValid(target: number, nums: number[]): boolean {
+    let op1works = false;
+    let op2Works = false;
+
+    if (nums.length == 1) {
+        return nums[0] == target;
+    }
     
+    if (target % nums[0] == 0) {
+        op1works = checkIfValid(target / nums[0], nums.slice(1));
+        if (op1works) return true;
+    }
+    op2Works = checkIfValid(target - nums[0], nums.slice(1));
+    return op1works || op2Works;
+}
+
+console.log(`==== ${day}: PART 1 ====`);
+
+timedExecute(() => {
+    let checkLines = lines.map(line => {
+        let parts = line.split(":");
+        let target = parseInt(parts[0]);
+        let nums = parts[1].trim().split(" ").map(n => parseInt(n)).reverse();
+        return { target, nums, valid: checkIfValid(target, nums) };
+    });
+    // add the target of the lines where the checkWorks is true
+    let sum = checkLines.filter(cl => cl.valid).reduce((acc, cl) => acc + cl.target, 0);
+    console.log(`Sum of targets that are valid: ${sum}`);
 });
